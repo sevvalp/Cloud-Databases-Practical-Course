597887478b28e114d207520d4b2ed12283516695
Basak Akan <basak.akan@tum.de>
2021-11-13 10:14:26 +0100

    Disk Manager Todo's done.

--- src/main/java/de/tum/i13/server/disk/DiskManager.java
+++ src/main/java/de/tum/i13/server/disk/DiskManager.java
@@ -1,12 +1,12 @@
 package de.tum.i13.server.disk;
 
 import de.tum.i13.server.kv.KVMessage;
+import de.tum.i13.server.kv.ServerMessage;
+import de.tum.i13.shared.B64Util;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 import java.util.logging.Logger;
 
 import static de.tum.i13.shared.Constants.TELNET_ENCODING;
@@ -14,27 +14,28 @@ import static de.tum.i13.shared.Constants.TELNET_ENCODING;
 public class DiskManager implements IDiskManager {
 
     private final static Logger LOGGER = Logger.getLogger(DiskManager.class.getName());
-    private final ConcurrentMap<String, Object> writeLocks = new ConcurrentHashMap<>();
     private String w_path;
 
     private static class Holder {
         private static final DiskManager MANAGER = new DiskManager();
     }
 
-    private DiskManager(){};
+    private DiskManager() {
+    }
 
     /**
      * Returns the DiskManager instance.
      *
-     * @return  The DiskManager instance.
+     * @return The DiskManager instance.
      */
-    public static DiskManager getInstance(){
+    public static DiskManager getInstance() {
         return Holder.MANAGER;
     }
 
     /**
      * Initializes the DiskManager
-     * @param path  Path where the files shall be stored.
+     *
+     * @param path Path where the files shall be stored.
      */
     public void initDiskManager(String path) {
         this.w_path = path;
@@ -43,7 +44,7 @@ public class DiskManager implements IDiskManager {
     /**
      * Returns the path where the files will be stored.
      *
-     * @return  Path where the files will be stored.
+     * @return Path where the files will be stored.
      */
     public String getW_path() {
         return w_path;
@@ -57,41 +58,39 @@ public class DiskManager implements IDiskManager {
      */
     @Override
     public KVMessage writeContent(KVMessage msg) {
-        // TODO: return KVMessage
-        // TODO: check if manager is initialized (i.e. we have a path where to store the files)
-        // TODO: return error if msg status is not PUT
-        // TODO: Catch Exceptions here and return PUT_ERROR KVMessage
-        // TODO: In case of error, supply message to be printed to client as value
-        // TODO: Use B64Util.encode(<message>) to encode the message
 
+        if (this.w_path == null)
+            return new ServerMessage(KVMessage.StatusType.PUT_ERROR, msg.getKey(), B64Util.b64encode("Disk Manager is not yet initialized!"));
+
+        if (msg.getStatus() != KVMessage.StatusType.PUT)
+            return new ServerMessage(KVMessage.StatusType.PUT_ERROR, msg.getKey(), B64Util.b64encode("KVMessage does not have correct status!"));
+
+        LOGGER.info(String.format("Put into disk: <%s, %s>", msg.getKey(), msg.getValue()));
+        KVMessage.StatusType status;
         try {
-            String filepath = getW_path()+ msg.getKey() + ".dat";
-            LOGGER.info("Writing file path " + filepath);
+            String filepath = getW_path() + msg.getKey() + ".dat";
             File nFile = new File(filepath);
-            //  Object lock = writeLocks.get(filepath);
 
-            //not sure if we need that
-            //   synchronized (lock) {
-                //if file exists delete old value
-                if (nFile.exists())
-                    new FileOutputStream(filepath).close();
-                else {//else create a new file
-                    if(!nFile.getParentFile().exists())
-                        new File(getW_path()).mkdirs();
-                    nFile.createNewFile();
-                }
+            //if file exists delete old value
+            if (nFile.exists()) {
+                new FileOutputStream(filepath).close();
+                status = KVMessage.StatusType.PUT_UPDATE;
+            } else {//else create a new file
+                if (nFile.createNewFile())
+                    status = KVMessage.StatusType.PUT_SUCCESS;
+                else
+                    return new ServerMessage(KVMessage.StatusType.PUT_ERROR, msg.getKey(), B64Util.b64encode("Error occurred while writing to disk!"));
+            }
+            FileOutputStream fos = new FileOutputStream(filepath);
+            fos.write(msg.getValue().getBytes());
+            fos.close();
 
-                try (FileOutputStream fos = new FileOutputStream(filepath)) {
-                    fos.write(msg.getValue().getBytes());
-                } catch (IOException exception) {
-                    LOGGER.severe("IO Exception while writing to file. " +  filepath);
-                }
-         //   }
         } catch (IOException e) {
-            LOGGER.severe("IO Exception while writing to file."+  e.getMessage());
+            LOGGER.info("IO Exception while writing to file.");
+            return new ServerMessage(KVMessage.StatusType.PUT_ERROR, msg.getKey(), B64Util.b64encode("Error occurred while writing to disk!"));
         }
 
-        return null;
+        return new ServerMessage(status, msg.getKey(), msg.getValue());
     }
 
     /**
@@ -102,30 +101,30 @@ public class DiskManager implements IDiskManager {
      */
     @Override
     public KVMessage readContent(KVMessage msg) {
-        // TODO: return KVMessage
-        // TODO: check if manager is initialized (i.e. we have a path where to store the files)
-        // TODO: return error if msg status is not GET
-        // TODO: Catch Exceptions here and return GET_ERROR
 
-            ByteArrayOutputStream data = new ByteArrayOutputStream();
-            String filepath = getW_path() + msg.getKey() + ".dat";
+        if (this.w_path == null)
+            return new ServerMessage(KVMessage.StatusType.GET_ERROR, msg.getKey(), B64Util.b64encode("Disk Manager is not yet initialized!"));
 
-            File nFile = new File(filepath);
+        if (msg.getStatus() != KVMessage.StatusType.GET)
+            return new ServerMessage(KVMessage.StatusType.GET_ERROR, msg.getKey(), B64Util.b64encode("KVMessage does not have correct status!"));
 
-            if (nFile.exists()) {
-                try {
-                    byte[] value = Files.readAllBytes(Paths.get(filepath));
-                    // TODO: return KVMessage
-                    //return new String(value,TELNET_ENCODING);
-                    return null;
-                } catch (IOException e) {
-                    LOGGER.severe("Error during getting value "+ e.getMessage());
-                }
-            } else{
-                LOGGER.severe("File not found on path: " + filepath);
+        LOGGER.info(String.format("Getting value from disk for %s", msg.getKey()));
+        String filepath = getW_path() + msg.getKey() + ".dat";
+        File nFile = new File(filepath);
+
+        if (nFile.exists()) {
+            try {
+                byte[] value = Files.readAllBytes(Paths.get(filepath));
+                return new ServerMessage(KVMessage.StatusType.GET_SUCCESS, msg.getKey(), new String(value, TELNET_ENCODING));
+            } catch (IOException e) {
+                LOGGER.info("IOException occurred while reading the from disk.");
+                return new ServerMessage(KVMessage.StatusType.GET_ERROR, msg.getKey(), B64Util.b64encode("Exception occurred while getting key!"));
             }
+        } else {
+            LOGGER.info("Key not in disk");
+            return new ServerMessage(KVMessage.StatusType.GET_ERROR, msg.getKey(), B64Util.b64encode("Key not in disk!"));
+        }
 
-        return  null;
     }
 
     /**
@@ -136,22 +135,33 @@ public class DiskManager implements IDiskManager {
      */
     @Override
     public KVMessage deleteContent(KVMessage msg) {
-        // TODO: return KVMessage with the result
-        // TODO: check if manager is initialized (i.e. we have a path where to store the files)
-        // TODO: return error if msg status is not DELETE
-        // TODO: return error if file could not be found
-        LOGGER.info("Deleting key from disk.");
 
-        String filepath = getW_path()+ msg.getKey() + ".dat";
+        if (this.w_path == null)
+            return new ServerMessage(KVMessage.StatusType.DELETE_ERROR, msg.getKey(), B64Util.b64encode("Disk Manager is not yet initialized!"));
+
+        if (msg.getStatus() != KVMessage.StatusType.DELETE)
+            return new ServerMessage(KVMessage.StatusType.DELETE_ERROR, msg.getKey(), B64Util.b64encode("KVMessage does not have correct status!"));
+
+        LOGGER.info(String.format("Deleting key from disk: %s", msg.getKey()));
+
+        String filepath = getW_path() + msg.getKey() + ".dat";
         File dFile = new File(filepath);
-        if(dFile.exists()){
-            dFile.delete();
-            LOGGER.info(msg.getKey() + " successfully deleted from disk.");
-        }else{
-            LOGGER.info(msg.getKey() + " file could not be found.");
+        try {
+            if (dFile.exists()) {
+                byte[] value = Files.readAllBytes(Paths.get(filepath));
+                if (dFile.delete())
+                    return new ServerMessage(KVMessage.StatusType.DELETE_SUCCESS, msg.getKey(), new String(value, TELNET_ENCODING));
+                else
+                    return new ServerMessage(KVMessage.StatusType.DELETE_ERROR, msg.getKey(), B64Util.b64encode("Exception occurred while deleting key!"));
+            } else {
+                LOGGER.info(" Key not in disk");
+                return new ServerMessage(KVMessage.StatusType.DELETE_ERROR, msg.getKey(), B64Util.b64encode("Key not in disk!"));
+            }
+        } catch (IOException e) {
+            LOGGER.info("IOException occurred while deleting key the from disk.");
+            return new ServerMessage(KVMessage.StatusType.DELETE_ERROR, msg.getKey(), B64Util.b64encode("Exception occurred while deleting key!"));
         }
 
-        return null;
     }
 
 }
