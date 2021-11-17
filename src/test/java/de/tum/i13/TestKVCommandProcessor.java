ce6adf06abf80e77bf15081958fcdda063a97009
Basak Akan <basak.akan@tum.de>
2021-11-17 21:30:54 +0100

    KVCommandProcessor correct parsing of put, get, delete tests added

--- src/test/java/de/tum/i13/TestKVCommandProcessor.java
+++ src/test/java/de/tum/i13/TestKVCommandProcessor.java
@@ -1,21 +1,60 @@
 package de.tum.i13;
 
 import de.tum.i13.server.kv.KVCommandProcessor;
+import de.tum.i13.server.kv.KVMessage;
 import de.tum.i13.server.kv.KVStore;
 import org.junit.jupiter.api.Test;
-
+import org.mockito.ArgumentCaptor;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class TestKVCommandProcessor {
+
 
     @Test
     public void correctParsingOfPut() throws Exception {
 
         KVStore kv = mock(KVStore.class);
         KVCommandProcessor kvcp = new KVCommandProcessor(kv);
-        //kvcp.process("put key hello");
+        kvcp.process(null, "put key hello");
 
-        //verify(kv).put("key", "hello");
+        ArgumentCaptor<KVMessage> argument = ArgumentCaptor.forClass(KVMessage.class);
+        verify(kv).put(argument.capture());
+        assertEquals("hello", argument.getValue().getValue());
+        assertEquals("key", argument.getValue().getKey());
+        assertEquals(KVMessage.StatusType.PUT, argument.getValue().getStatus());
+
+    }
+
+    @Test
+    public void correctParsingOfGet() throws Exception {
+
+        KVStore kv = mock(KVStore.class);
+        KVCommandProcessor kvcp = new KVCommandProcessor(kv);
+        kvcp.process(null, "get key");
+
+        ArgumentCaptor<KVMessage> argument = ArgumentCaptor.forClass(KVMessage.class);
+        verify(kv).get(argument.capture());
+        assertEquals("key", argument.getValue().getKey());
+        assertNull(argument.getValue().getValue());
+        assertEquals(KVMessage.StatusType.GET, argument.getValue().getStatus());
+
+    }
+
+    @Test
+    public void correctParsingOfDelete() throws Exception {
+
+        KVStore kv = mock(KVStore.class);
+        KVCommandProcessor kvcp = new KVCommandProcessor(kv);
+        kvcp.process(null, "delete key");
+
+        ArgumentCaptor<KVMessage> argument = ArgumentCaptor.forClass(KVMessage.class);
+        verify(kv).delete(argument.capture());
+        assertNull(argument.getValue().getValue());
+        assertEquals("key", argument.getValue().getKey());
+        assertEquals(KVMessage.StatusType.DELETE, argument.getValue().getStatus());
+
     }
 }
